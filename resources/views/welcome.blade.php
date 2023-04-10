e658ac75aaeddf337770dba65fad267843e225a1
abdelkrim-jouidri <abdelkrimjouidri@gmail.com>
2023-04-10 09:27:58 +0000

    added table to dashboard

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -104,9 +104,10 @@
             </div>
         </div>
     </nav>
-    <div class="  flex">
-        <aside class="w-[13%] mr-5 ml-1 border-r-2  bg-gray-800 h-screen rounded-md  shadow-2xl">
-            <ul class="flex justiy-center items-center flex-col mt-8    ">
+
+    <div class="  flex container mx-auto overflow-x-scroll ">
+        <!-- <aside class="w-[13%] mr-5 ml-1 border-r-2  bg-gray-800 h-screen rounded-md  shadow-2xl ">
+            <ul class="flex gap-6 justify-center flex-col mt-8  h-[80%]  ">
                 <li class="text-sm font-medium  hover:rounded-md hover:text-white hover:bg-gray-700 px-4 py-3 text-center text-gray-300">
                     <a href="">Dashboard</a>
                 </li>
@@ -120,10 +121,68 @@
                     <a href="">My details</a>
                 </li>
             </ul>
-        </aside>
+        </aside> -->
 
-        <div class="primary  flex-1 flex justify-center items-center">
-            lorem2  
+        <div class="primary  flex-1 flex justify-center items-center w-[60%]">
+            <table class="min-w-full divide-y divide-gray-200">
+                <thead class="bg-gray-50">
+                    <tr>
+                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Name
+                    </th>
+                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Title
+                    </th>
+                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Status
+                    </th>
+                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Role
+                    </th>
+                    <th scope="col" class="relative px-6 py-3">
+                        <span class="sr-only">Edit</span>
+                    </th>
+                    </tr>
+                </thead>
+                <tbody class="bg-white divide-y divide-gray-200">
+                    <tr>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                        <div class="flex items-center">
+                        <div class="flex-shrink-0 h-10 w-10">
+                            <img class="h-10 w-10 rounded-full" src="https://via.placeholder.com/150" alt="">
+                        </div>
+                        <div class="ml-4">
+                            <div class="text-sm font-medium text-gray-900">
+                            Jane Doe
+                            </div>
+                            <div class="text-sm text-gray-500">
+                            jane.doe@example.com
+                            </div>
+                        </div>
+                        </div>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                        <div class="text-sm text-gray-900">Full Stack Developer</div>
+                        <div class="text-sm text-gray-500">Web Development</div>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                        <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                        Active
+                        </span>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                        Developer
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                        <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                    </td>
+                    </tr>
+
+                    <!-- More rows... -->
+
+                </tbody>
+            </table>
+ 
         </div>
     </div>
 
